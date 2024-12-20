df39f4b599133357666ea45f0c85078749cdb7cc
Ravi Kumar Jha <91972538+rkj6124@users.noreply.github.com>
2024-12-20 21:27:18 +0530

    Update truth_set/python/structure/solution-approach/change-function/app.py

--- truth_set/python/structure/solution-approach/change-function/app.py
+++ truth_set/python/structure/solution-approach/change-function/app.py
@@ -6,11 +6,6 @@ class User:
         self.id = id
         self.name = name
         self.email = email
-class User_1:
-    def __init__(self, id: int, name: str, email: str):
-        self.id = id
-        self.name = name
-        self.email = email
 
 db = Database()
 
